807cff260ae3b630b8ae8ffd04548292101ca6c8
Leonardo Faneze <leofaneze14@gmail.com>
2022-12-06 23:48:38 -0300

    Print Board

--- Chess.java
+++ Chess.java
@@ -1,7 +1,7 @@
 public class Chess {
     public static void main(String[] args) {
         
-        Board board = new Board(8, 8);
-        System.out.println(board);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
